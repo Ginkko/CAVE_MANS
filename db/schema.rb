040ea0719c0cc4c8184c8e62a1685e4538efc0c3
Alec Arme & Jim Patt <student@epicodus.com>
2015-07-06 10:17:21 -0700

    Update users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706165046) do
+ActiveRecord::Schema.define(version: 20150706171543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "updated_at"
+    t.datetime "created_at"
   end
 
 end
