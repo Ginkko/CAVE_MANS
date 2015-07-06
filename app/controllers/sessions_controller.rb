b19774699d4de6e5453123c09c64d66e120d4b57
Alec Arme & Jim Patt <student@epicodus.com>
2015-07-06 11:59:41 -0700

    Add navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,4 +12,10 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "YOU LEAVE CAVE. BYE."
+    redirect_to root_path
+  end
+
 end
